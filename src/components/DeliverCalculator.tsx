6c300e8836a40260e6f36262e8f219f0a909c977
Hao Chen <mmkrty@gmail.com>
2023-02-06 15:41:48 +0100

    adjust some styles

--- src/components/DeliverCalculator.tsx
+++ src/components/DeliverCalculator.tsx
@@ -13,7 +13,7 @@ const DeliverCalculator = () => {
   const [deliveryFee, setDeliveryFee] = useState<number>(0);
 
   return (
-    <div className="bg-[#001464] font-inter py-5 px-5 xs:mx-2 w-full h-full xs:w-96 xs:h-auto">
+    <div className="bg-[#001464] font-inter py-5 px-5 xs:mx-2 w-full h-full xs:w-96 xs:h-auto xs:rounded-sm">
       <h1 className="text-[#2ED7FE] text-4xl font-pacifico text-center">
         Deliver Fee Calculator
       </h1>
@@ -22,7 +22,7 @@ const DeliverCalculator = () => {
         setInputs={setInputs}
         setDeliveryFee={setDeliveryFee}
       />
-      <p className="text-xl text-center text-white">
+      <p className="text-2xl text-center text-white">
         Delivery price: {deliveryFee} €
       </p>
     </div>
